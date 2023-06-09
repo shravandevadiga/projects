6630c7fb7eb4ca6971434de78b9e59037fcb3a6b
shravandevadiga <103760555+shravandevadiga@users.noreply.github.com>
2023-06-09 15:56:30 +0530

    Add files via upload

---
+++ springboot_demo_app/springboot_demo_app/src/main/java/org/jsp/springboot_demo_app/dto/Demo.java
@@ -0,0 +1,26 @@
+package org.jsp.springboot_demo_app.dto;
+
+public class Demo {
+	private int id;
+	private String name;
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
